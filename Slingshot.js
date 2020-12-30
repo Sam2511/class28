c131dea7c088b695585b659206fc7e3c46b1a0c8
Samarth <misrarashi30@gmail.com>
2020-12-30 19:54:46 +0530

    slingshot effect given

---
+++ Slingshot.js
@@ -0,0 +1,31 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+        if(this.chain.bodyA) {  //only if bodyA is not nul
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.chain.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    fly(){
+        //bird gets detached from the slingshot
+        this.chain.bodyA  = null;
+    
+    }
+    
+}
+
+//Empty/Nothing - Null
+
+
